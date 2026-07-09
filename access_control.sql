f7cb7d02629e659280b41e1b7b05ea24b3685d53
sunilnandoofficial-debug <sunilnando.official@gmail.com>
2026-07-09 11:45:30 +0000

    change1

---
+++ access_control.sql
@@ -0,0 +1,17 @@
+CREATE USER tiku PASSWORD='StrongPass123';
+CREATE ROLE analyst_role;
+
+-- Grant warehouse access
+GRANT USAGE ON WAREHOUSE compute_wh TO ROLE analyst_role;
+
+-- Grant database access
+GRANT USAGE ON DATABASE sunil_test TO ROLE analyst_role;
+
+-- Grant schema access
+GRANT USAGE ON SCHEMA sunil_test.staging TO ROLE analyst_role;
+
+-- Grant table access
+GRANT SELECT ON ALL TABLES IN SCHEMA sunil_test.staging TO ROLE analyst_role;
+
+-- Assign role to user
+GRANT ROLE analyst_role TO USER tiku;
